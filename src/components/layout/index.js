30fbc763ac22932575be92308cece06ed09a8719
coolgoagle <102777764+coolgoagle@users.noreply.github.com>
2022-07-21 13:10:08 -0500

    Update index.js

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -1,12 +1,10 @@
 import Header from 'components/layout/header';
-import Footer from 'components/layout/footer';
 
 export default function Layout({ children }) {
 	return (
 		<>
 			<Header />
 			<main>{children}</main>
-			<Footer />
 		</>
 	);
 }
